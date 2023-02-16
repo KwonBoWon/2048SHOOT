ec33dadee1e87f1867a0513526a914ef3fa61292
BoWon <bogod1207@naver.com>
2023-02-16 22:01:54 +0900

    Add hp

--- My2048SHOOT/Assets/Script/OutBox.cs
+++ My2048SHOOT/Assets/Script/OutBox.cs
@@ -1,13 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class OutBox : MonoBehaviour
 {
-    public List <GameObject> outObj = new List <GameObject>();
-    // Start is called before the first frame update
-
-
-
+    //public List<GameObject> outObj = new List<GameObject>();
+    public GameObject hpTextobj;
+    public TextMeshProUGUI hpText;
     
+    
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+
+        if (collision.collider.CompareTag("cube"))
+        {
+            Debug.Log("Drop");
+            float hp = float.Parse(hpText.text);
+            string cubeNum = collision.gameObject.name;
+
+            float cubeNumf =  CubeManager.StringToInt(cubeNum) ;
+            cubeNumf = Mathf.Pow(2, cubeNumf);
+
+            hpText.text = (hp - cubeNumf).ToString();
+
+            Cube cube2 = collision.gameObject.GetComponent<Cube>();
+            Destroy(collision.gameObject);
+        }
+    }
+
+
 }

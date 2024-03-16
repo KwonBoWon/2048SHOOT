cb8872c0390bdedbdc9e800a16061e1a76ded154
KwonBoWon <bogod1207@naver.com>
2024-03-17 02:14:49 +0900

    Add comments

--- My2048SHOOT/Assets/Script/DataManager.cs
+++ My2048SHOOT/Assets/Script/DataManager.cs
@@ -19,9 +19,11 @@ public class DataManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     void Start()
     {
-        path = Path.Combine(Application.dataPath, "Data.json");
+        path = Path.Combine(Application.dataPath, "Data.json"); // json 데이터 저장 경로
     }
-
+    /// <summary>
+    /// 씬의 큐브 데이터들 모두 가져오기
+    /// </summary>
     public void GetCubeDataList()
     {
         cubeDataList = new List<CubeData>();
@@ -31,7 +33,9 @@ public class DataManager : MonoBehaviour
             cubeDataList.Add(cubeData);
         }
     }
-    
+    /// <summary>
+    /// 저장하기
+    /// </summary>
     public void SaveData()
     {
         GetCubeDataList();
@@ -40,7 +44,7 @@ public class DataManager : MonoBehaviour
         try
         {
             string json = JsonUtility.ToJson(data);
-            File.WriteAllText(path, json);
+            File.WriteAllText(path, json); // 파일 쓰기
             Debug.Log("데이터 저장 완료");
         }
         catch (Exception e)
@@ -48,27 +52,30 @@ public class DataManager : MonoBehaviour
             Debug.LogError("데이터 저장 실패: " + e.Message);
         }
     }
+    /// <summary>
+    /// 로드하기
+    /// </summary>
     public void LoadData()
     {
         if (File.Exists(path))
         {
-            string json = File.ReadAllText(path);
+            string json = File.ReadAllText(path); // 파일 읽기
             data = JsonUtility.FromJson<Data>(json);
             foreach (var cube in CubeManager.cubeList)
             {
-                Destroy(cube);
+                Destroy(cube); // 큐브 모두 삭제
             }
 
             CubeManager.cubeList = new List<GameObject>();
             scoreText.text = data.score.ToString();
             hpText.text = data.hp.ToString();
             core.transform.rotation = data.coreRotation;
-            //Math.Log(y, 2) - 1
+            
             foreach (var cd in data.cubeDataList)
             {
                 GameObject nowCube = Instantiate(
                     CubeManager.preCubes[(int)Math.Log(int.Parse(cd.name.Replace("(Clone)", "")), 2) -1],
-                    cd.position, cd.rotation);
+                    cd.position, cd.rotation); // 프리펩에서 가져오기
                 CubeManager.cubeList.Add(nowCube);
             }
 
@@ -79,13 +86,14 @@ public class DataManager : MonoBehaviour
         }
     }
 }
+
 [System.Serializable]
 public class Data
 {
-    public int score;
-    public int hp;
-    public Quaternion coreRotation;
-    public List<CubeData> cubeDataList;
+    public int score; // 점수
+    public int hp; // 체력
+    public Quaternion coreRotation; // 벽 로테이션
+    public List<CubeData> cubeDataList; // 큐브 정보 리스트
     public Data(int score, int hp, Quaternion coreRotation,
         List<CubeData> cubeDataList)
     {
@@ -98,9 +106,9 @@ public class Data
 [System.Serializable]
 public class CubeData
 {
-    public Vector3 position;
-    public Quaternion rotation;
-    public string name;
+    public Vector3 position; // 포지션
+    public Quaternion rotation; // 로테이션
+    public string name; // 이름(프리펩가져오기위해)
 
     public CubeData(Vector3 position, Quaternion rotation, string name)
     {
